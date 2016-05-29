91f967f1c7d1f05e167d632d3d2b4eeec7e48c51
Zhang He <zhang_he06@163.com>
2016-05-29 22:32:53 +0800

    新增回到顶部

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -25,15 +25,15 @@ $('[rel="tooltip"]').tooltip();
 
 
 /* 附加导航 */
-$(function(){
+$(function () {
     // 滚动页面侧边悬浮动态导航宽度控制
-    $(window).resize(function(){
+    $(window).resize(function () {
         $('#affix_nav_ul').width($('#affix_nav_ul').parent().width());
         //console.log($('.container').width());
         // 如果屏幕宽度小于940px 隐藏侧边导航
-        if ($('.container').width() <= 940){
+        if ($('.container').width() <= 940) {
             $('#affix_nav_ul').hide();
-        }else {
+        } else {
             $('#affix_nav_ul').show();
         }
     });
@@ -43,10 +43,23 @@ $(function(){
 
 /* 按钮加载状态 */
 // html button 标签其中 autocomplete="off" 属性是针对FF浏览器在页面加载之后，禁用状态不会自动解除用的。
-$(function() {
-    $(".btn-load").click(function(){
-        $(this).button('loading').delay(1000).queue(function() {
+$(function () {
+    $(".btn-load").click(function () {
+        $(this).button('loading').delay(1000).queue(function () {
             //$(this).button('reset');
         });
     });
 });
+
+
+/* 返回顶部 */
+$('#top-link').click(function () {
+    $('html,body').animate({scrollTop: 0}, 'slow');
+    return false;
+});
+if (($(window).height() + 100) < $(document).height()) {
+    $('#top-link-block').removeClass('hidden').affix({
+        // how far to scroll down before link "slides" into view
+        offset: {top: 100}
+    });
+}
