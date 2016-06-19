b99122b9c2239b763b3a924cbd5fcb068ea60333
Zhang He <zhang_he06@163.com>
2016-06-19 22:31:37 +0800

    更新代码生成工具

--- app/models.py
+++ app/models.py
@@ -8,6 +8,16 @@ Base = db.Model
 metadata = Base.metadata
 
 
+def to_dict(self):
+    """
+    model 对象转 字典
+    model_obj.to_dict()
+    """
+    return {c.name: getattr(self, c.name, None) for c in self.__table__.columns}
+
+Base.to_dict = to_dict
+
+
 class Author(Base):
     __tablename__ = 'author'
 
