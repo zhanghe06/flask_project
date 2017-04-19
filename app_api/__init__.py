be37609cd5b0ecf3d034324b29b85e9753a306fb
Zhang He <zhang_he06@163.com>
2017-04-19 17:48:17 +0800

    更新项目结构

---
+++ app_api/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: zhanghe
+@software: PyCharm
+@file: __init__.py.py
+@time: 2017/4/19 下午2:24
+"""
+
+
+def func():
+    pass
+
+
+class Main(object):
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
